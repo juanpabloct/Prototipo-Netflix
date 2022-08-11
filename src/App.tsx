4e762dd66db18b3cb736692b4ee3a647bb6ca221
Juanpabloct <juanptorres2003@gmail.com>
2022-08-11 16:07:02 -0500

    Se añadio nuevos estilos al app y se ubico en compomponentes y variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { Grid } from "@mui/material";
-import { url } from "./connect api/urlApi";
 import { genres } from "./Var Global/genres";
-import { Genres } from "./types/genres/TypeGenres";
 import { DataMovies } from "./types/TypeDataMovie";
 import { Main } from "./components/main/main";
 import { useDispatch, useSelector } from "react-redux";
-import { setData, setLoading } from "./reducers/valuesMovieReducers";
-import { AnyAction, Dispatch } from "@reduxjs/toolkit";
-import { reducer } from "./main";
-export const getData = async (
-  genres: Genres[],
-  dispatch: Dispatch<AnyAction>
-) => {
-  let value = await Promise.all(
-    genres.map(async (gender) => {
-      const data: any = url(
-        `/discover/movie?api_key=20f8bdc37feb2aa2ca15659e13d847d3&with_genres=${gender.id}`
-      ).then((res: any) => {
-        return { gender: gender.name, movies: res.data.results };
-      });
-      return data;
-    })
-  );
-  dispatch(setData(value));
-  dispatch(setLoading());
-};
+import { reducer } from './main';
+import { ComplementImage } from "./Var Global/complementImage";
 function App() {
   const { loading, data, error, showMovie } = useSelector(
     (state: reducer) => state.data
   );
+  const {backdrop_path}=useSelector((state:reducer)=>{
+    return state.data.showMovie
+  })
   return (
-    <Grid container direction={"column"} justifyContent={"center"}>
+    <>
+      <div className="imgUrl" style={{backgroundImage:`url(${ComplementImage +backdrop_path})`}}></div>
+    <Grid container direction={"column"} justifyContent={"center"} position={"relative"}>
       <Navbar />
       {!loading && <Main />}
     </Grid>
+    </>
   );
 }
 
