34273bac140a71ff270ac74c2ec74d6f9d369369
juanpabloct <juanptorres2003@gmail.com>
2023-03-05 02:46:52 -0500

    Mejora en el scroll para todo dipo de pantalla

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,7 +1,7 @@
-import { Grid, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setShowMovies } from "../../reducers/valuesMovieReducers";
 import { Movie, TypeMoviesInfo } from "../../types/data/index";
@@ -10,20 +10,22 @@ import "./querys.css";
 export const Carousel = ({
   gender,
   width = "100vw",
-  avance_slider = 19.5,
-  limiteSlider = 300,
+  style = "imgMovie",
 }: {
   gender: TypeMoviesInfo;
   width?: string;
-  limiteSlider?: number;
-  avance_slider?: number;
+  style?: string;
 }) => {
+  const contain = useRef<HTMLDivElement>(null);
   const dispatch = useDispatch();
   const [avanzar, setAvanzar] = useState(0);
-  const valor = `-${avanzar}vw`;
-
+  const valor = `-${avanzar}px`;
+  const image = useRef<HTMLImageElement>(null);
+  const sizeImage = image?.current?.width ?? 1;
+  const multiplicar = gender.movies.length % 2 === 0 ? 2 : 1;
+  const ValorLimite = (sizeImage + 4) * (gender.movies.length - 1);
   return (
-    <div className="contain" style={{ width }}>
+    <div className="contain" ref={contain} style={{ width: width }}>
       <h1 className="gender">{gender.name}</h1>
       <div className="parentSlider">
         <IconButton
@@ -34,9 +36,21 @@ export const Carousel = ({
             zIndex: "30",
             top: "40%",
           }}
-          onClick={() =>
-            setAvanzar(avanzar === 0 ? limiteSlider : avanzar - avance_slider)
-          }
+          onClick={() => {
+            gender;
+            if (sizeImage && contain.current && ValorLimite) {
+              const resultadoRetroceso = avanzar - sizeImage * multiplicar;
+              const retroceder =
+                resultadoRetroceso < 0 ? 0 : resultadoRetroceso;
+              setAvanzar(
+                avanzar <= 0
+                  ? ValorLimite
+                  : avanzar < sizeImage
+                  ? 0
+                  : retroceder
+              );
+            }
+          }}
         >
           <ChevronLeftIcon sx={{ color: "##685454a8" }} />
         </IconButton>
@@ -47,7 +61,8 @@ export const Carousel = ({
           {gender.movies.map((movie: Movie) => {
             return (
               <img
-                className="imgMovie"
+                ref={image}
+                className={style}
                 key={movie.id}
                 src={ComplementImage + movie.poster_path}
                 alt={movie.title}
@@ -67,9 +82,19 @@ export const Carousel = ({
             zIndex: "30",
             top: "40%",
           }}
-          onClick={() =>
-            setAvanzar(avanzar > limiteSlider ? 0 : avanzar + avance_slider)
-          }
+          onClick={() => {
+            if (sizeImage && contain.current && ValorLimite) {
+              const widthContain = contain.current.clientWidth;
+              const resultadoAvance = avanzar + sizeImage * multiplicar;
+              setAvanzar(
+                avanzar === 0
+                  ? resultadoAvance
+                  : avanzar >= ValorLimite
+                  ? 0
+                  : resultadoAvance
+              );
+            }
+          }}
         >
           <ChevronRightIcon sx={{ color: "##685454a8" }} />
         </IconButton>
