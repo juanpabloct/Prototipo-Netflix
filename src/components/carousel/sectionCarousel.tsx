364ba041f6e738b6c23e86f1e8387b1153e40979
juanpabloct <juanptorres2003@gmail.com>
2023-03-05 02:47:28 -0500

    Quito contenedor inncesario

--- src/components/carousel/sectionCarousel.tsx
+++ src/components/carousel/sectionCarousel.tsx
@@ -6,11 +6,7 @@ export const SectionCarousel = () => {
   return (
     <div className="contain_all_carousel">
       {data.map((movie, id) => {
-        return (
-          <div key={id} className="containCarouselGender">
-            <Carousel gender={movie} width={"100%"}/>;
-          </div>
-        );
+        return <Carousel gender={movie} width={"100%"} />;
       })}
     </div>
   );
