59bf499743b1781ed00e844024b6a9711c5cd084
juanpabloct <juanptorres2003@gmail.com>
2023-03-05 02:48:28 -0500

    Agtrego para que tomara todo el tamaño del padre

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,7 +5,7 @@ import "./main.css";
 import { SectionCarousel } from "../carousel/sectionCarousel";
 export const Main = () => {
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <Grid container spacing={1} marginTop={"3rem"}>
         <Grid item xs={12}>
           <SectionCarousel />
